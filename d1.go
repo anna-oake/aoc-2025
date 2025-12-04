72b6282ee1ecc02c5da8a41c43c582d3252a61a9
Anna Oake <anna@oa.ke>
2025-12-04 13:05:05 +0100

    add day 1 part 2

--- d1.go
+++ d1.go
@@ -33,3 +33,43 @@ func (*methods) D1P1(input string) string {
 
 	return strconv.Itoa(cnt)
 }
+
+// i know the following is CRINGE. i promise i'll come back to this and fix it later
+func (*methods) D1P2(input string) string {
+	lines := strings.Split(input, "\n")
+	pos := 50
+	var cnt int
+	for _, line := range lines {
+		line = strings.TrimPrefix(line, "R")
+		line = strings.ReplaceAll(line, "L", "-")
+		if line == "" {
+			continue
+		}
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+		neg := false
+		if num < 0 {
+			neg = true
+			num *= -1
+		}
+		for i := 0; i < num; i++ {
+			if neg {
+				pos--
+			} else {
+				pos++
+			}
+			if pos == 100 {
+				pos = 0
+			} else if pos == -1 {
+				pos = 99
+			}
+			if pos == 0 {
+				cnt++
+			}
+		}
+	}
+
+	return strconv.Itoa(cnt)
+}
